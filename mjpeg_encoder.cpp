raspberrypi/rpicam-apps

commit b20dc097621a31a1edf0bd26d39aae0ee1398e2f

fix buildroot crosscompile (#1)
* cmake: remove unsafe host include path for libdrm (use pkgconfig instead)

Signed-off-by: Peter Seiderer <ps.report@gmx.net>

* cmake: add option ENABLE_COMPILE_FLAGS_FOR_TARGET

Add cmake option ENABLE_COMPILE_FLAGS_FOR_TARGET, defaulting to armv8
enabling the previous hard coded compiler options.

Run cmake with -DENABLE_COMPILE_FLAGS_FOR_TARGET=disabled for no extra
compiler options.

Fixes:

  aarch64-buildroot-linux-gnu-g++.br_real: error: unrecognized command-line option ‘-mfpu=neon-fp-armv8’

Signed-off-by: Peter Seiderer <ps.report@gmx.net>

* libcamera_vid: fix getline call (needs size_t)

Fixes:

.../libcamera-apps-custom/libcamera_vid.cpp:39:26: error: cannot convert ‘unsigned int*’ to ‘size_t*’ {aka ‘long unsigned int*’}
   39 |    getline(&user_string, &len, stdin);
      |                          ^~~~
      |                          |
      |                          unsigned int*

Signed-off-by: Peter Seiderer <ps.report@gmx.net>

* libcamera_still: fix getline call (needs size_t)

Fixes:

.../build/libcamera-apps-custom/libcamera_still.cpp:152:26: error: cannot convert ‘unsigned int*’ to ‘size_t*’ {aka ‘long unsigned int*’}
  152 |    getline(&user_string, &len, stdin);
      |                          ^~~~
      |                          |
      |                          unsigned int*

Signed-off-by: Peter Seiderer <ps.report@gmx.net>

* libcamera_app: fix int64_t constants

Fixes:

.../host/aarch64-buildroot-linux-gnu/sysroot/usr/include/libcamera/libcamera/controls.h: In instantiation of ‘void libcamera::ControlList::set(const libcamera::Control<T>&, const std::initializer_list<_Up>&) [with T = libcamera::Span<const long int>; V = long long int]’:
.../libcamera_app.hpp:331:18:   required from ‘void LibcameraApp<OPTIONS>::StartCamera() [with OPTIONS = StillOptions]’
.../libcamera_jpeg.cpp:35:18:   required from here

mjpeg_encoder.cpp
@@ -12,6 +12,12 @@
 
 #include "mjpeg_encoder.hpp"
 
+#if JPEG_LIB_VERSION_MAJOR > 9 || (JPEG_LIB_VERSION_MAJOR == 9 && JPEG_LIB_VERSION_MINOR >= 4)
+typedef size_t jpeg_mem_len_t;
+#else
+typedef unsigned long jpeg_mem_len_t;
+#endif
+
 MjpegEncoder::MjpegEncoder(VideoOptions const &options)
 	: Encoder(options), abort_(false), index_(0)
 {
@@ -44,7 +50,7 @@ int MjpegEncoder::EncodeBuffer(int fd, size_t size,
 }
 
 void MjpegEncoder::encodeJPEG(struct jpeg_compress_struct &cinfo, EncodeItem &item,
-							  uint8_t *&encoded_buffer, long unsigned int &buffer_len)
+							  uint8_t *&encoded_buffer, size_t &buffer_len)
 {
 	// Copied from YUV420_to_JPEG_fast in jpeg.cpp.
     cinfo.image_width = item.width;
@@ -54,11 +60,13 @@ void MjpegEncoder::encodeJPEG(struct jpeg_compress_struct &cinfo, EncodeItem &it
 	cinfo.restart_interval = 0;
 
     jpeg_set_defaults(&cinfo);
-	cinfo.raw_data_in = true;
+	cinfo.raw_data_in = TRUE;
     jpeg_set_quality(&cinfo, options_.quality, TRUE);
 	encoded_buffer = nullptr;
 	buffer_len = 0;
-    jpeg_mem_dest(&cinfo, &encoded_buffer, &buffer_len);
+    jpeg_mem_len_t jpeg_mem_len;
+    jpeg_mem_dest(&cinfo, &encoded_buffer, &jpeg_mem_len);
+    buffer_len = jpeg_mem_len;
     jpeg_start_compress(&cinfo, TRUE);
 
 	int stride2 = item.stride / 2;
@@ -145,7 +153,7 @@ void MjpegEncoder::encodeThread(int num)
 
 		// Encode the buffer.
 		uint8_t *encoded_buffer = nullptr;
-		long unsigned int buffer_len = 0;
+		size_t buffer_len = 0;
 		auto start_time = std::chrono::high_resolution_clock::now();
 		encodeJPEG(cinfo, encode_item, encoded_buffer, buffer_len);
 		encode_time += (std::chrono::high_resolution_clock::now() - start_time);

CMakeLists.txt
@@ -13,7 +13,13 @@ set (CMAKE CXX_FLAGS "-Wall -Wextra -pedantic -Wno-unused-parameter -faligned-ne
 add_definitions(-Wfatal-errors)
 add_definitions(-Wno-psabi)
 add_definitions(-DBOOST_LOG_DYN_LINK)
-add_definitions(-mfpu=neon-fp-armv8 -ftree-vectorize)
+
+IF (NOT ENABLE_COMPILE_FLAGS_FOR_TARGET)
+  set(ENABLE_COMPILE_FLAGS_FOR_TARGET "armv8")
+endif()
+if ("${ENABLE_COMPILE_FLAGS_FOR_TARGET}" STREQUAL "armv8")
+  add_definitions(-mfpu=neon-fp-armv8 -ftree-vectorize)
+endif()
 
 project(common)
 add_library(common output.cpp file_output.cpp net_output.cpp circular_output.cpp egl_preview.cpp drm_preview.cpp jpeg.cpp)
@@ -25,8 +31,9 @@ project(libcamera-still)
 
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(CAMERA REQUIRED camera)
+pkg_check_modules(LIBDRM REQUIRED libdrm)
 
-include_directories(. "${CAMERA_INCLUDE_DIRS}" /usr/include/libdrm)
+include_directories(. "${CAMERA_INCLUDE_DIRS}" "${LIBDRM_INCLUDE_DIRS}")
 
 find_package( Boost REQUIRED COMPONENTS program_options )
 
@@ -43,28 +50,28 @@ message(STATUS "${EPOXY_LIBRARY}")
 find_library(DRM_LIBRARY libdrm.so REQUIRED)
 message(STATUS "${DRM_LIBRARY}")
 
-find_library(IPA_RPI_LIBRARY ipa_rpi.so PATHS "${CAMERA_LIBRARY_DIRS}/libcamera" REQUIRED)
-message(STATUS "${IPA_RPI_LIBRARY}")
-
 add_executable(libcamera-still libcamera_still.cpp yuv.cpp dng.cpp png.cpp bmp.cpp)
-target_link_libraries(libcamera-still common "${LIBCAMERA_LIBRARY}" "${IPA_RPI_LIBRARY}" exif ${Boost_LIBRARIES} jpeg tiff png pthread "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
+target_link_libraries(libcamera-still common "${LIBCAMERA_LIBRARY}" exif ${Boost_LIBRARIES} jpeg tiff png pthread "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
 
 project(libcamera-vid)
 
 add_executable(libcamera-vid libcamera_vid.cpp)
-target_link_libraries(libcamera-vid encoders common "${LIBCAMERA_LIBRARY}" "${IPA_RPI_LIBRARY}" ${Boost_LIBRARIES} pthread jpeg "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
+target_link_libraries(libcamera-vid encoders common "${LIBCAMERA_LIBRARY}" exif ${Boost_LIBRARIES} pthread jpeg "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
 
 project(libcamera-hello)
 
 add_executable(libcamera-hello libcamera_hello.cpp)
-target_link_libraries(libcamera-hello common "${LIBCAMERA_LIBRARY}" "${IPA_RPI_LIBRARY}" ${Boost_LIBRARIES} pthread "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
+target_link_libraries(libcamera-hello common "${LIBCAMERA_LIBRARY}" exif ${Boost_LIBRARIES} jpeg pthread "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
 
 project(libcamera-raw)
 
 add_executable(libcamera-raw libcamera_raw.cpp)
-target_link_libraries(libcamera-raw encoders common "${LIBCAMERA_LIBRARY}" "${IPA_RPI_LIBRARY}" jpeg ${Boost_LIBRARIES} pthread "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
+target_link_libraries(libcamera-raw encoders common "${LIBCAMERA_LIBRARY}" exif jpeg ${Boost_LIBRARIES} pthread "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
 
 project(libcamera-jpeg)
 
 add_executable(libcamera-jpeg libcamera_jpeg.cpp)
-target_link_libraries(libcamera-jpeg common "${LIBCAMERA_LIBRARY}" "${IPA_RPI_LIBRARY}" exif jpeg ${Boost_LIBRARIES} pthread "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
+target_link_libraries(libcamera-jpeg common "${LIBCAMERA_LIBRARY}" exif jpeg ${Boost_LIBRARIES} pthread "${X11_LIBRARIES}" "${EPOXY_LIBRARY}" "${DRM_LIBRARY}")
+
+install(TARGETS common encoders LIBRARY DESTINATION lib ARCHIVE DESTINATION lib)
+install(TARGETS libcamera-still libcamera-vid libcamera-hello libcamera-raw libcamera-jpeg RUNTIME DESTINATION bin)

circular_output.cpp
@@ -69,7 +69,7 @@ void CircularOutput::outputBuffer(void *mem, size_t size, int64_t timestamp_us, 
 		cb_.Read([&dst](void *src, int n) { memcpy(dst, src, n); dst += n; }, sizeof(header));
 		cb_.Skip((header.length + ALIGN - 1) & ~(ALIGN - 1));
 	}
-	Header header = { size, flags & FLAG_KEYFRAME, timestamp_us };
+	Header header = { static_cast<unsigned int>(size), flags & FLAG_KEYFRAME, timestamp_us };
 	cb_.Write(&header, sizeof(header));
 	cb_.Write(mem, size);
 	cb_.Pad(pad);

egl_preview.cpp
@@ -216,7 +216,8 @@ void EglPreview::makeWindow(char const *name)
 	if (!eglGetConfigAttrib(egl_display_, config, EGL_NATIVE_VISUAL_ID, &vid))
 		throw std::runtime_error("eglGetConfigAttrib() failed\n");
 
-	XVisualInfo visTemplate = { .visualid = (long unsigned int)vid };
+	XVisualInfo visTemplate = {};
+	visTemplate.visualid = (VisualID)vid;
 	int num_visuals;
 	XVisualInfo *visinfo = XGetVisualInfo(display_, VisualIDMask,
                                          &visTemplate, &num_visuals);

jpeg.cpp
@@ -23,6 +23,12 @@
 
 #include "still_options.hpp"
 
+#if JPEG_LIB_VERSION_MAJOR > 9 || (JPEG_LIB_VERSION_MAJOR == 9 && JPEG_LIB_VERSION_MINOR >= 4)
+typedef size_t jpeg_mem_len_t;
+#else
+typedef unsigned long jpeg_mem_len_t;
+#endif
+
 using namespace libcamera;
 
 typedef int (*ExifReadFunction)(char const *, unsigned char *);
@@ -249,7 +255,7 @@ static void YUYV_to_JPEG(const uint8_t *input,
 						 const int input_width, const int input_height, const int stride,
 						 const int output_width, const int output_height,
 						 const int quality, const unsigned int restart,
-						 uint8_t *&jpeg_buffer, long unsigned int &jpeg_len)
+						 uint8_t *&jpeg_buffer, jpeg_mem_len_t &jpeg_len)
 {
     struct jpeg_compress_struct cinfo;
     struct jpeg_error_mgr jerr;
@@ -304,7 +310,7 @@ static void YUYV_to_JPEG(const uint8_t *input,
 static void YUV420_to_JPEG_fast(const uint8_t *input,
 								const int width, const int height, const int stride,
 								const int quality, const unsigned int restart,
-								uint8_t *&jpeg_buffer, long unsigned int &jpeg_len)
+								uint8_t *&jpeg_buffer, jpeg_mem_len_t &jpeg_len)
 {
     struct jpeg_compress_struct cinfo;
     struct jpeg_error_mgr jerr;
@@ -319,7 +325,7 @@ static void YUV420_to_JPEG_fast(const uint8_t *input,
 	cinfo.restart_interval = restart;
 
     jpeg_set_defaults(&cinfo);
-	cinfo.raw_data_in = true;
+	cinfo.raw_data_in = TRUE;
     jpeg_set_quality(&cinfo, quality, TRUE);
 	jpeg_buffer = NULL;
 	jpeg_len = 0;
@@ -377,7 +383,7 @@ static void YUV420_to_JPEG(const uint8_t *input,
 						   const int input_width, const int input_height, const int stride,
 						   const int output_width, const int output_height,
 						   const int quality, const unsigned int restart,
-						   uint8_t *&jpeg_buffer, long unsigned int &jpeg_len)
+						   uint8_t *&jpeg_buffer, jpeg_mem_len_t &jpeg_len)
 {
 	if (input_width == output_width && input_height == output_height)
 	{
@@ -446,7 +452,7 @@ static void YUV_to_JPEG(PixelFormat const &pixel_format, const uint8_t *input,
 						const int input_width, const int input_height, const int stride,
 						const int output_width, const int output_height,
 						const int quality, const unsigned int restart,
-						uint8_t *&jpeg_buffer, long unsigned int &jpeg_len)
+						uint8_t *&jpeg_buffer, jpeg_mem_len_t &jpeg_len)
 {
 	if (pixel_format == libcamera::formats::YUYV)
 		YUYV_to_JPEG(input, input_width, input_height, stride,
@@ -467,7 +473,7 @@ static void create_exif_data(PixelFormat const &pixel_format,
 							 ControlList const &metadata,
 							 std::string const &cam_name, StillOptions const &options,
 							 uint8_t *&exif_buffer, unsigned int &exif_len,
-							 uint8_t *&thumb_buffer, long unsigned int &thumb_len)
+							 uint8_t *&thumb_buffer, jpeg_mem_len_t &thumb_len)
 {
 	exif_buffer = nullptr;
 	ExifData *exif = nullptr;
@@ -616,7 +622,7 @@ void jpeg_save(std::vector<void *> const &mem, int w, int h, int stride,
 
 		// Make all the EXIF data, which includes the thumbnail.
 
-		long unsigned int thumb_len;
+		jpeg_mem_len_t thumb_len;
 		unsigned int exif_len;
 		create_exif_data(pixel_format, mem, w, h, stride, metadata, cam_name, options,
 						 exif_buffer, exif_len, thumb_buffer, thumb_len);
@@ -624,7 +630,7 @@ void jpeg_save(std::vector<void *> const &mem, int w, int h, int stride,
 		// Make the full size JPEG (could probably be more efficient if we had
 		// YUV422 or YUV420 planar format).
 
-		long unsigned int jpeg_len;
+		jpeg_mem_len_t jpeg_len;
 		YUV_to_JPEG(pixel_format, (uint8_t *)(mem[0]), w, h, stride, w, h,
 					 options.quality, options.restart, jpeg_buffer, jpeg_len);
 		if (options.verbose)

libcamera_app.hpp
@@ -328,7 +328,7 @@ public:
 		if (!controls_.contains(controls::FrameDurations))
 		{
 			if (still_stream_)
-				controls_.set(controls::FrameDurations, { 100LL, 1000000000LL });
+				controls_.set(controls::FrameDurations, { INT64_C(100), INT64_C(1000000000) });
 			else if (options.framerate > 0)
 			{
 				int64_t frame_time = 1000000 / options.framerate; // in us

libcamera_still.cpp
@@ -148,7 +148,7 @@ static int get_key_or_signal(StillOptions const &options, pollfd p[1])
 		if (p[0].revents & POLLIN)
 		{
 			char *user_string = nullptr;
-			unsigned int len;
+			size_t len;
 			getline(&user_string, &len, stdin);
 			key = user_string[0];
 		}

libcamera_vid.cpp
@@ -35,7 +35,7 @@ static int get_key_or_signal(VideoOptions const &options, pollfd p[1])
 		if (p[0].revents & POLLIN)
 		{
 			char *user_string = nullptr;
-			unsigned int len;
+			size_t len;
 			getline(&user_string, &len, stdin);
 			key = user_string[0];
 		}

commit 1b500a7954557dce8f1db63e03ec8eeeca0d98ec

mjpeg_encoder: fix bug that truncated jpegs to 4096 bytes
Fixing up some type warnings had caused the jpeg length not to be
returned correctly from the encodeJPEG method.

Test script has also been altered to spot this.

Fixes: b20dc097621a ("fix buildroot crosscompile (#1)")
Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

mjpeg_encoder.cpp
@@ -66,7 +66,6 @@ void MjpegEncoder::encodeJPEG(struct jpeg_compress_struct &cinfo, EncodeItem &it
 	buffer_len = 0;
     jpeg_mem_len_t jpeg_mem_len;
     jpeg_mem_dest(&cinfo, &encoded_buffer, &jpeg_mem_len);
-    buffer_len = jpeg_mem_len;
     jpeg_start_compress(&cinfo, TRUE);
 
 	int stride2 = item.stride / 2;
@@ -113,6 +112,7 @@ void MjpegEncoder::encodeJPEG(struct jpeg_compress_struct &cinfo, EncodeItem &it
 	}
 
     jpeg_finish_compress(&cinfo);
+    buffer_len = jpeg_mem_len;
 }
 
 void MjpegEncoder::encodeThread(int num)

test.py
@@ -257,7 +257,8 @@ def test_vid(dir):
                                          logfile)
     check_retcode(retcode, "test_vid: segment test")
     check_time(time_taken, 2, 5, "test_vid: segment test")
-    check_size(os.path.join(dir, 'test035.jpg'), 1024, "test_vid: segment test")
+    # A bug in commit b20dc097621a trunctated each jpg to 4096 bytes, so check against 4100:
+    check_size(os.path.join(dir, 'test035.jpg'), 4100, "test_vid: segment test")
 
     # "circular test". Test circular buffer (really we should wait for it to wrap...)
     print("    circular test")