1ff1dad16bf2f8611b45e6e302ba85fbec957b19
David Plowman <david.plowman@raspberrypi.com>
2021-07-08 08:51:32 +0100

    libcamera_app.h no longer needs to include preview.hpp
    It can just be forward referenced. Additionally, the derived class
    headers can now be merged into their cpp files (so egl_preview.hpp has
    disappeared into the top of egl_preview.cpp and so forth).
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- egl_preview.cpp
+++ egl_preview.cpp
@@ -5,11 +5,62 @@
  * egl_preview.cpp - X/EGL-based preview window.
  */
 
+#include <map>
+#include <string>
+
 #include <libdrm/drm_fourcc.h>
 
-#include "egl_preview.hpp"
-
+#include <X11/Xlib.h>
 #include <X11/Xutil.h>
+
+#include <epoxy/egl.h>
+#include <epoxy/gl.h>
+
+#include "preview.hpp"
+
+class EglPreview : public Preview
+{
+public:
+	EglPreview(Options const *options);
+	~EglPreview();
+	virtual void SetInfoText(const std::string &text) override;
+	// Display the buffer. You get given the fd back in the BufferDoneCallback
+	// once its available for re-use.
+	virtual void Show(int fd, size_t size, int width, int height, int stride) override;
+	// Reset the preview window, clearing the current buffers and being ready to
+	// show new ones.
+	virtual void Reset() override;
+	// Check if the window manager has closed the preview.
+	virtual bool Quit() override;
+
+private:
+	struct Buffer
+	{
+		Buffer() : fd(-1) {}
+		int fd;
+		size_t size;
+		int width;
+		int height;
+		int stride;
+		GLuint texture;
+	};
+	void makeWindow(char const *name);
+	void makeBuffer(int fd, size_t size, int width, int height, int stride, Buffer &buffer);
+	::Display *display_;
+	EGLDisplay egl_display_;
+	Window window_;
+	EGLContext egl_context_;
+	EGLSurface egl_surface_;
+	std::map<int, Buffer> buffers_; // map the DMABUF's fd to the Buffer
+	int last_fd_;
+	bool first_time_;
+	Atom wm_delete_window_;
+	// size of preview window
+	int x_;
+	int y_;
+	int width_;
+	int height_;
+};
 
 static GLint compile_shader(GLenum target, const char *source)
 {
