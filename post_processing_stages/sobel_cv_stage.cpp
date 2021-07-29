27162583881f8afee34b1407122c73b774a92e38
Shonak Joshi <shonak.joshi@raspberrypi.com>
2021-07-29 13:33:41 +0100

    Post-processing: Add Sobel stage
    The "Sobel" stage requires OpenCV and uses it to apply a Sobel filter
    to the camera images.
    
    Signed-off-by: Shonak Joshi <shonak.joshi@raspberrypi.com>

---
+++ post_processing_stages/sobel_cv_stage.cpp
@@ -0,0 +1,99 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2021, Raspberry Pi (Trading) Limited
+ *
+ * sobel_cv_stage.cpp - Sobel filter implementation, using OpenCV
+ */
+
+#include <libcamera/stream.h>
+
+#include "../core/libcamera_app.hpp"
+#include "../core/post_processing_stage.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+
+using namespace cv;
+
+using Stream = libcamera::Stream;
+
+class SobelCvStage : public PostProcessingStage
+{
+public:
+	SobelCvStage(LibcameraApp *app) : PostProcessingStage(app) {}
+
+	char const *Name() const override;
+
+	void Read(boost::property_tree::ptree const &params) override;
+
+	void Configure();
+
+	void Process(CompletedRequest &completed_request);
+
+private:
+	Stream *stream_;
+	int ksize_ = 3;
+};
+
+#define NAME "sobel_cv"
+
+char const *SobelCvStage::Name() const
+{
+	return NAME;
+}
+
+void SobelCvStage::Read(boost::property_tree::ptree const &params)
+{
+	ksize_ = params.get<int16_t>("ksize", 3);
+}
+
+void SobelCvStage::Configure()
+{
+	stream_ = app_->GetMainStream();
+	if (!stream_ || stream_->configuration().pixelFormat != libcamera::formats::YUV420)
+		throw std::runtime_error("SobelCvStage: only YUV420 format supported");
+}
+
+void SobelCvStage::Process(CompletedRequest &completed_request)
+{
+	int w, h, stride;
+	app_->StreamDimensions(stream_, &w, &h, &stride);
+	libcamera::Span<uint8_t> buffer = app_->Mmap(completed_request.buffers[stream_])[0];
+	uint8_t *ptr = (uint8_t *)buffer.data();
+
+	//Everything beyond this point is image processing...
+
+	uint8_t value = 128;
+	int num = (stride * h) / 2;
+	Mat src = Mat(h, w, CV_8U, ptr, stride);
+	int scale = 1;
+	int delta = 0;
+	int ddepth = CV_16S;
+
+	memset(ptr + stride * h, value, num);
+
+	// Remove noise by blurring with a Gaussian filter ( kernal size = 3 )
+	GaussianBlur(src, src, Size(3, 3), 0, 0, BORDER_DEFAULT);
+
+	Mat grad_x, grad_y;
+
+	//Scharr(src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT);
+	Sobel(src, grad_x, ddepth, 1, 0, ksize_, scale, delta, BORDER_DEFAULT);
+	//Scharr(src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT);
+	Sobel(src, grad_y, ddepth, 0, 1, ksize_, scale, delta, BORDER_DEFAULT);
+
+	// converting back to CV_8U
+	convertScaleAbs(grad_x, grad_x);
+	convertScaleAbs(grad_y, grad_y);
+
+	//weight the x and y gradients and add their magnitudes
+	addWeighted(grad_x, 0.5, grad_y, 0.5, 0, src);
+}
+
+static PostProcessingStage *Create(LibcameraApp *app)
+{
+	return new SobelCvStage(app);
+}
+
+static RegisterStage reg(NAME, &Create);
