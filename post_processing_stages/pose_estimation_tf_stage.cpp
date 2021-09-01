e8d35db3bf5449183057142bd91bfc897d68c56c
David Plowman <david.plowman@raspberrypi.com>
2021-09-01 10:11:35 +0100

    post-processing: Refactor PoseEstimationTfStage to use TfStage base class
    Mostly this just involves the removal of code that is now in the base class.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- post_processing_stages/pose_estimation_tf_stage.cpp
+++ post_processing_stages/pose_estimation_tf_stage.cpp
@@ -5,195 +5,80 @@
  * pose_estimation_tf_stage - pose estimator
  */
 
-#include <algorithm>
-#include <fstream>
-#include <functional>
-#include <iostream>
-#include <map>
-#include <memory>
-#include <queue>
-#include <string>
-#include <vector>
+#include "tf_stage.hpp"
 
-#include <libcamera/stream.h>
-
-#include "core/libcamera_app.hpp"
-#include "core/post_processing_stage.hpp"
-
-#include "tensorflow/lite/builtin_op_data.h"
-#include "tensorflow/lite/c/common.h"
-#include "tensorflow/lite/interpreter.h"
-#include "tensorflow/lite/kernels/register.h"
-#include "tensorflow/lite/string_util.h"
-
-using Stream = libcamera::Stream;
-
-struct TfSettings
-{
-	std::string model_name;
-	int number_of_threads;
-	int refresh_rate;
-};
-
-constexpr int IMAGE_WIDTH = 258;
-constexpr int IMAGE_HEIGHT = 258;
-constexpr int IMAGE_CHANNELS = 3;
 constexpr int FEATURE_SIZE = 17;
 constexpr int HEATMAP_DIMS = 9;
 
-class PoseEstimationTfStage : public PostProcessingStage
+#define NAME "pose_estimation_tf"
+
+class PoseEstimationTfStage : public TfStage
 {
 public:
-	PoseEstimationTfStage(LibcameraApp *app) : PostProcessingStage(app) {}
+	// The model we use expects 257x257 images. Really.
+	PoseEstimationTfStage(LibcameraApp *app) : TfStage(app, 257, 257) { config_ = std::make_unique<TfConfig>(); }
+	char const *Name() const override { return NAME; }
 
-	char const *Name() const override;
+protected:
+	void readExtras(boost::property_tree::ptree const &params) override;
 
-	void Read(boost::property_tree::ptree const &params);
+	void checkConfiguration() override;
 
-	void Configure();
+	// Retrieve the various joint coordinates and confidences from the model.
+	void interpretOutputs() override;
 
-	bool Process(CompletedRequest &completed_request);
-
-	void Stop();
+	// Attach results as metadata.
+	void applyResults(CompletedRequest &completed_request) override;
 
 private:
-	void initialize();
-
-	void runInference();
-
-	std::vector<float> yuvToRgb(uint8_t *mem, int w, int h, int stride);
-
-	Stream *lores_stream_;
-	Stream *main_stream_;
-	int w_, h_, stride_;
-	int main_w_, main_h_, main_stride_;
-	TfSettings settings_;
-	std::unique_ptr<tflite::FlatBufferModel> model_;
-	std::unique_ptr<tflite::Interpreter> interpreter_;
-	std::unique_ptr<std::future<void>> future_ptr_;
-	std::mutex future_ptr_mutex_;
-	std::mutex output_results_mutex_;
-	std::vector<float> tensor_input_;
 	std::vector<libcamera::Point> heats_;
 	std::vector<float> confidences_;
 	std::vector<libcamera::Point> locations_;
 };
 
-#define NAME "pose_estimation_tf"
-
-char const *PoseEstimationTfStage::Name() const
+void PoseEstimationTfStage::readExtras([[maybe_unused]] boost::property_tree::ptree const &params)
 {
-	return NAME;
+	// Actually we don't read anything, but we can check the output tensor dimensions.
+	int output = interpreter_->outputs()[0];
+	TfLiteIntArray *dims = interpreter_->tensor(output)->dims;
+	// Causes might include loading the wrong model.
+	if (dims->data[0] != 1 || dims->data[1] != HEATMAP_DIMS || dims->data[2] != HEATMAP_DIMS ||
+		dims->data[3] != FEATURE_SIZE)
+		throw std::runtime_error("PoseEstimationTfStage: Unexpected output dimensions");
 }
 
-void PoseEstimationTfStage::Read(boost::property_tree::ptree const &params)
+void PoseEstimationTfStage::checkConfiguration()
 {
-	settings_.refresh_rate = params.get<int>("refresh_rate", 10);
-	settings_.model_name = params.get<std::string>(
-		"model_file", "/home/pi/models/posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite");
-	settings_.number_of_threads = params.get<int>("number_of_threads", 3);
-
-	initialize();
+	if (!main_stream_)
+		throw std::runtime_error("PoseEstimationTfStage: Main stream is required");
 }
 
-void PoseEstimationTfStage::initialize()
+void PoseEstimationTfStage::applyResults(CompletedRequest &completed_request)
 {
-	model_ = tflite::FlatBufferModel::BuildFromFile(settings_.model_name.c_str());
-	std::cout << "Loaded model " << settings_.model_name << std::endl;
-
-	model_->error_reporter();
-	tflite::ops::builtin::BuiltinOpResolver resolver;
-
-	tflite::InterpreterBuilder(*model_, resolver)(&interpreter_);
-	if (!interpreter_)
-		throw std::runtime_error("Failed to construct interpreter");
-
-	std::cout << "Input size " << interpreter_->inputs().size() << std::endl;
-
-	if (settings_.number_of_threads != -1)
-		interpreter_->SetNumThreads(settings_.number_of_threads);
-
-	if (interpreter_->AllocateTensors() != kTfLiteOk)
-		throw std::runtime_error("Failed to allocate tensors");
-}
-
-void PoseEstimationTfStage::Configure()
-{
-	lores_stream_ = app_->LoresStream();
-	if (lores_stream_)
-	{
-		app_->StreamDimensions(lores_stream_, &w_, &h_, &stride_);
-		std::cout << w_ << " " << h_ << std::endl;
-		if (w_ != IMAGE_WIDTH || h_ != IMAGE_HEIGHT)
-			throw std::runtime_error("Lores width and height must be 258");
-	}
-	main_stream_ = app_->GetMainStream();
-	if (main_stream_)
-	{
-		app_->StreamDimensions(main_stream_, &main_w_, &main_h_, &main_stride_);
-		std::cout << main_w_ << " " << main_h_ << std::endl;
-	}
-	else
-	{
-		throw std::runtime_error("Main Stream is null");
-	}
-}
-
-bool PoseEstimationTfStage::Process(CompletedRequest &completed_request)
-{
-	if (!lores_stream_)
-		return false;
-	{
-		std::unique_lock<std::mutex> lck(future_ptr_mutex_);
-		if (completed_request.sequence % settings_.refresh_rate == 0 &&
-			(!future_ptr_ || future_ptr_->wait_for(std::chrono::seconds(0)) == std::future_status::ready))
-		{
-			libcamera::Span<uint8_t> buffer = app_->Mmap(completed_request.buffers[lores_stream_])[0];
-			uint8_t *ptr = buffer.data();
-
-			tensor_input_ = yuvToRgb(ptr, w_, h_, stride_);
-
-			future_ptr_ = std::make_unique<std::future<void>>();
-			*future_ptr_ = std::move(std::async(std::launch::async, [this] { runInference(); }));
-		}
-	}
-
-	std::unique_lock<std::mutex> lock(output_results_mutex_);
 	completed_request.post_process_metadata.Set("pose_estimation.locations", locations_);
 	completed_request.post_process_metadata.Set("pose_estimation.confidences", confidences_);
-
-	return false;
 }
 
-void PoseEstimationTfStage::runInference()
+void PoseEstimationTfStage::interpretOutputs()
 {
 	// This code has been adapted from the "Qengineering/TensorFlow_Lite_Pose_RPi_32-bits" repository and can be
 	// found here: "https://github.com/Qengineering/TensorFlow_Lite_Pose_RPi_32-bits/blob/master/Pose_single.cpp"
-	for (int i = 0; i < tensor_input_.size(); i++)
-	{
-		interpreter_->typed_tensor<float>(interpreter_->inputs()[0])[i] = static_cast<float>(tensor_input_[i]);
-	}
-
-	interpreter_->Invoke();
-
-	std::unique_lock<std::mutex> lock(output_results_mutex_);
-
 	float *heatmaps = interpreter_->tensor(interpreter_->outputs()[0])->data.f;
 	float *offsets = interpreter_->tensor(interpreter_->outputs()[1])->data.f;
 
-	float confidence_temp;
-	int j;
 	heats_.clear();
+	confidences_.clear();
+	locations_.clear();
 
 	for (int i = 0; i < FEATURE_SIZE; i++)
 	{
-		confidence_temp = heatmaps[i];
+		float confidence_temp = heatmaps[i];
 		libcamera::Point heat_coord;
 		for (int y = 0; y < HEATMAP_DIMS; y++)
 		{
 			for (int x = 0; x < HEATMAP_DIMS; x++)
 			{
-				j = FEATURE_SIZE * (HEATMAP_DIMS * y + x) + i;
+				int j = FEATURE_SIZE * (HEATMAP_DIMS * y + x) + i;
 				if (heatmaps[j] > confidence_temp)
 				{
 					confidence_temp = heatmaps[j];
@@ -205,8 +90,6 @@ void PoseEstimationTfStage::runInference()
 		heats_.push_back(heat_coord);
 		confidences_.push_back(confidence_temp);
 	}
-
-	locations_.clear();
 
 	for (int i = 0; i < FEATURE_SIZE; i++)
 	{
@@ -220,32 +103,6 @@ void PoseEstimationTfStage::runInference()
 	}
 }
 
-std::vector<float> PoseEstimationTfStage::yuvToRgb(uint8_t *mem, int w, int h, int stride)
-{
-	int dst;
-	//our Low-Res input (YUV420) has to be 258*258 but the PoseNet model requires 257*257 so we trim the ends.
-	std::vector<float> output((h - 1) * (w - 1) * 3);
-
-	for (int y = 0; y < h - 1; y++)
-	{
-		for (int x = 0; x < w - 1; x++)
-		{
-			uint8_t *Y = mem + y * stride + x;
-			uint8_t *U = mem + (h * stride) + ((int)floor(y / 2)) * (stride / 2) + ((int)floor(x / 2));
-			uint8_t *V = mem + (5 * h * stride) / 4 + ((int)floor(y / 2)) * (stride / 2) + ((int)floor(x / 2));
-			dst = (x + y * (w - 1)) * 3;
-
-			int YY = *Y, UU = *U, VV = *V;
-
-			output[dst] = (std::clamp<int>((YY + (1.402 * (VV - 128))), 0, 255) - 127.5) / 127.5;
-			output[dst + 1] = (std::clamp<int>((YY - 0.345 * (UU - 128) - 0.714 * (VV - 128)), 0, 255) - 127.5) / 127.5;
-			output[dst + 2] = (std::clamp<int>((YY + 1.771 * (UU - 128)), 0, 255) - 127.5) / 127.5;
-		}
-	}
-
-	return output;
-}
-
 static PostProcessingStage *Create(LibcameraApp *app)
 {
 	return new PoseEstimationTfStage(app);
@@ -253,8 +110,3 @@ static PostProcessingStage *Create(LibcameraApp *app)
 
 static RegisterStage reg(NAME, &Create);
 
-void PoseEstimationTfStage::Stop()
-{
-	if (future_ptr_)
-		future_ptr_->wait();
-}
