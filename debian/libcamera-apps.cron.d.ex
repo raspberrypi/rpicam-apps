84f9ab4529975e1d5b3fbe8d7d844dea3fa6c607
Serge Schneider <serge@raspberrypi.com>
2021-09-02 17:07:57 +0100

    Update debian/ template files

--- debian/libcamera-apps.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libcamera-apps package
-#
-0 4	* * *	root	[ -x /usr/bin/libcamera-apps_maintenance ] && /usr/bin/libcamera-apps_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libcamera-apps):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libcamera-apps" command="/usr/bin/libcamera-apps"
