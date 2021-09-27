49be8cba58f3ef9f366d021dde33294bff75ea92
David Plowman <david.plowman@raspberrypi.com>
2021-09-27 09:30:18 +0100

    libcamera-detect: Fix bug shown up by CompleteRequest move vs. copy fixes
    The CompletedRequest gets (correctly) moved now rather than copied, so
    you can't look in the metadata after sending it to the preview, you
    must do it before.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- apps/libcamera_detect.cpp
+++ apps/libcamera_detect.cpp
@@ -74,14 +74,17 @@ static void event_loop(LibcameraDetectApp &app)
 			if (options->timeout && now - start_time > std::chrono::milliseconds(options->timeout))
 				return;
 
+			// Must check the metadata before sending to preview.
+			std::vector<Detection> detections;
+			bool detected = completed_request.sequence - last_capture_frame >= options->gap &&
+							completed_request.post_process_metadata.Get("object_detect.results", detections) == 0 &&
+							std::find_if(detections.begin(), detections.end(), [options](const Detection &d) {
+								return d.name.find(options->object) != std::string::npos;
+							}) != detections.end();
+
 			app.ShowPreview(completed_request, app.ViewfinderStream());
 
-			std::vector<Detection> detections;
-			if (completed_request.sequence - last_capture_frame >= options->gap &&
-				completed_request.post_process_metadata.Get("object_detect.results", detections) == 0 &&
-				std::find_if(detections.begin(), detections.end(), [options](const Detection &d) {
-					return d.name.find(options->object) != std::string::npos;
-				}) != detections.end())
+			if (detected)
 			{
 				app.StopCamera();
 				app.Teardown();
