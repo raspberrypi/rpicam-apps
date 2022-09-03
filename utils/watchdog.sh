0b701ba005b950e8b262b504c78a07f8b6418625
Thomas D <drebert@web.de>
2022-09-03 22:17:12 +0200

    debug Version.

--- utils/watchdog.sh
+++ utils/watchdog.sh
@@ -3,35 +3,35 @@
 datum=$(date +%Y%m%d_%H%M%S)
 PNAME="libcamera-detect"
 load="$(/usr/bin/ps -C ${PNAME} -o %cpu | tail -1)"
-echo "step 1" "$load"  >> /home/thomasd/cam8start.txt
+echo "step 1"
 b=10
 int=${load%.*}
-echo "step 2" "$int"  >> /home/thomasd/cam8start.txt
+echo "step 2"
 re='^[0-9]+$'
 
 if ! [[ $int =~ $re ]]
 then
    load2=1
-   echo "step 3" "$load2"  >> /home/thomasd/cam8start.txt
+   echo "step 3"
 fi
 if [[ $int =~ $re ]]
 then
    load2=$int
-   echo "step 4" "$load2"  >> /home/thomasd/cam8start.txt
+   echo "step 4"
 fi
 
-echo "step 5" "$load2"  >> /home/thomasd/cam8start.txt
-echo "step 6" "$int"
+echo "step 5"
+echo "step 6"
 if [ $load2 -lt $b ]
 then
   process="$(pidof libcamera-detect | wc -w)"
    if [ $process == 0 ];
    then
-     echo "step 7" "$int" "$process " "start" >> /home/thomasd/cam8start.txt
+     echo "step 7"
      /usr/local/bin/libcamera-detect -n -t 0 -o "%04d.jpg" --saturation 0.2 --contrast 1.3 --hflip --vflip --lores-width 450 --lores-height 300 --gap 10 --framerate 30 --verbose 0 --post-process-file /home/thomasd/object_detect_tf.json
    else
-     echo "stop 8" "$process " "stop" >> /home/thomasd/cam8start.txt
+     echo "stop 8"
      killall libcamera-detect
    fi
 fi
-echo "$datum" "step 9" "$load2" "$process " "---------------------------------------------" >> /home/thomasd/cam8start.txt
+echo "$datum" "step 9"
