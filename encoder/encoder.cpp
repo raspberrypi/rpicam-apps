213b166b7e48b3fca0a4aa172baaa773f1dd280f
Naushir Patuck <naush@raspberrypi.com>
2023-04-06 09:51:55 +0100

    encoder: Add logic to auto-detect H.264 encoder type
    Add a function to automatically select the H.264 codec based on the
    platform availability.  The hardware H.264 codec is preferred over the
    sofware x264/libav codec.
    
    The "auto" codec option is now the default.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- encoder/encoder.cpp
+++ encoder/encoder.cpp
@@ -6,6 +6,11 @@
  */
 
 #include <cstring>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+
+#include <linux/videodev2.h>
 
 #include "encoder.hpp"
 #include "h264_encoder.hpp"
@@ -16,12 +21,34 @@
 #include "libav_encoder.hpp"
 #endif
 
-Encoder *Encoder::Create(VideoOptions const *options, const StreamInfo &info)
+Encoder *h264_codec_select(VideoOptions *options, const StreamInfo &info)
+{
+	const char hw_codec[] = "/dev/video11";
+	struct v4l2_capability caps;
+	memset(&caps, 0, sizeof(caps));
+	int fd = open(hw_codec, O_RDWR, 0);
+	if (fd)
+	{
+		int ret = ioctl(fd, VIDIOC_QUERYCAP, &caps);
+		if (!ret && !strncmp((char *)caps.card, "bcm2835-codec-encode", sizeof(caps.card)))
+			return new H264Encoder(options, info);
+	}
+
+#if LIBAV_PRESENT
+	// No hardware codec available, use x264 through libav.
+	options->libav_video_codec = "libx264";
+	return new LibAvEncoder(options, info);
+#endif
+
+	throw std::runtime_error("Unable to find an appropriate H.264 codec");
+}
+
+Encoder *Encoder::Create(VideoOptions *options, const StreamInfo &info)
 {
 	if (strcasecmp(options->codec.c_str(), "yuv420") == 0)
 		return new NullEncoder(options);
 	else if (strcasecmp(options->codec.c_str(), "h264") == 0)
-		return new H264Encoder(options, info);
+		return h264_codec_select(options, info);
 #if LIBAV_PRESENT
 	else if (strcasecmp(options->codec.c_str(), "libav") == 0)
 		return new LibAvEncoder(options, info);
