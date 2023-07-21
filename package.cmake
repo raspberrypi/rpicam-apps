a2dd54563740e3ec0bf5b223d762072148d169a6
Naushir Patuck <naush@raspberrypi.com>
2023-07-21 09:50:36 +0100

    build: Remove cmake files
    The cmake has been deprecated for a while, now remove it entirely.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- package.cmake
+++
@@ -1,5 +0,0 @@
-# Script to generate a version string and save it to the package source root
-
-execute_process(COMMAND git rev-parse HEAD
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-                OUTPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/version.gen)
