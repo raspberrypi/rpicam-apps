05e9fb7118736f8633e798e5521578e4573e82ae
Naushir Patuck <naush@raspberrypi.com>
2024-01-26 10:16:32 +0000

    libav: Select the correct libav codec
    On Pi5, do not allow users to use the h264_v4l2m2m codec, as this will
    not work.  Instead, revert to "libx264" as a default codec.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- encoder/encoder.cpp
+++ encoder/encoder.cpp
@@ -44,6 +44,31 @@ Encoder *h264_codec_select(VideoOptions *options, const StreamInfo &info)
 	throw std::runtime_error("Unable to find an appropriate H.264 codec");
 }
 
+#if LIBAV_PRESENT
+Encoder *libav_codec_select(VideoOptions *options, const StreamInfo &info)
+{
+	if (options->libav_video_codec == "h264_v4l2m2m")
+	{
+		const char hw_codec[] = "/dev/video11";
+		struct v4l2_capability caps;
+		memset(&caps, 0, sizeof(caps));
+		int fd = open(hw_codec, O_RDWR, 0);
+		if (fd)
+		{
+			int ret = ioctl(fd, VIDIOC_QUERYCAP, &caps);
+			close(fd);
+			if (!ret && !strncmp((char *)caps.card, "bcm2835-codec-encode", sizeof(caps.card)))
+				return new LibAvEncoder(options, info);
+		}
+
+		// No h264_v4l2m2m libav codec available, use libx264 if nothing else is provided.
+		options->libav_video_codec = "libx264";
+	}
+
+	return new LibAvEncoder(options, info);
+}
+#endif
+
 Encoder *Encoder::Create(VideoOptions *options, const StreamInfo &info)
 {
 	if (strcasecmp(options->codec.c_str(), "yuv420") == 0)
@@ -52,7 +77,7 @@ Encoder *Encoder::Create(VideoOptions *options, const StreamInfo &info)
 		return h264_codec_select(options, info);
 #if LIBAV_PRESENT
 	else if (strcasecmp(options->codec.c_str(), "libav") == 0)
-		return new LibAvEncoder(options, info);
+		return libav_codec_select(options, info);
 #endif
 	else if (strcasecmp(options->codec.c_str(), "mjpeg") == 0)
 		return new MjpegEncoder(options);
