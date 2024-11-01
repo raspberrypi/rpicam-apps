c17a41641b2a289d013b69dfb1bfb3cd63cbaef4
Naushir Patuck <naush@raspberrypi.com>
2024-11-01 13:44:29 +0000

    utils: hailo: Add new Hailo 8 models
    Update the download scripts for Hailo 8 models where available for the
    existing post-processing stages.
    
    Download new Face landmarking models for the Hailo 8L.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- utils/download-hailo-models.sh
+++ utils/download-hailo-models.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 set -e
 
-networks=(
+declare -A networks=(
     # Image classification
-    https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/resnet_v1_50_h8l.hef
+    ["https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/resnet_v1_50_h8l.hef"]="resnet_v1_50_h8l.hef"
     # Yolov6 inference
-    https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov6n.hef
+    ["https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov6n.hef"]="yolov6n_h8l.hef"
+    ["https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/yolov6n.hef"]="yolov6n_h8.hef"
     # Yolov8 inference
-    https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_h8l.hef
+    ["https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_h8l.hef"]="yolov8s_h8l.hef"
+    ["https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/yolov8s.hef"]="yolov8s_h8.hef"
     # Yolov5 segmentation
-    https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov5n_seg_h8l_mz.hef
+    ["https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov5n_seg_h8l_mz.hef"]="yolov5n_seg_h8l_mz.hef"
+    ["https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/yolov5n_seg.hef"]="yolov5n_seg_h8.hef"
     # YoloX inference
-    https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolox_s_leaky_h8l_mz.hef
+    ["https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolox_s_leaky_h8l_mz.hef"]="yolox_s_leaky_h8l_rpi.hef"
     # Yolov8 pose
-    https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_pose_h8l_pi.hef
+    ["https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_pose_h8l_pi.hef"]="yolov8s_pose_h8l_pi.hef"
+    ["https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8/yolov8s_pose.hef"]="yolov8s_pose_h8.hef"
     # Yolov5 person/face inference
-    https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov5s_personface_h8l.hef
+    ["https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov5s_personface_h8l.hef"]="yolov5s_personface_h8l.hef"
+    # Face landmarking
+    ["https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8l/scrfd_2.5g.hef"]="scrfd_2.5g_h8l.hef"
 )
 
 if [ $# -ne 1 ]; then
@@ -26,6 +32,7 @@ fi
 dir=$1
 mkdir -p $dir
 
-for url in ${networks[@]}; do
-    wget -nv -N -P $dir $url
+for url in "${!networks[@]}"; do
+    filename="${networks[$url]}"
+    wget -nv -O "$dir/$filename" "$url"
 done
