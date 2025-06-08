raspberrypi/rpicam-apps

commit c52909638633213aed806ac3b8d3408905133ac7

core: Add "disable_rpi_features" meson option
Use this option to #ifdef out features that are only available in the
downstream RPi libcamera tree. This will allow rpicam-apps to build
and link with an upstream libcamera tree.

Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

core/rpicam_encoder.hpp
@@ -30,6 +30,7 @@ public:
 		encoder_->SetInputDoneCallback(std::bind(&RPiCamEncoder::encodeBufferDone, this, std::placeholders::_1));
 		encoder_->SetOutputReadyCallback(encode_output_ready_callback_);
 
+#ifndef DISABLE_RPI_FEATURES
 		// Set up the encode function to wait for synchronisation with another camera system,
 		// when this has been requested in the options.
 		VideoOptions const *options = GetOptions();
@@ -41,6 +42,7 @@ public:
 		else if (options->sync == 2)
 			cl.set(libcamera::controls::rpi::SyncMode, libcamera::controls::rpi::SyncModeClient);
 		SetControls(cl);
+#endif
 	}
 	// This is callback when the encoder gives you the encoded output data.
 	void SetEncodeOutputReadyCallback(EncodeOutputReadyCallback callback) { encode_output_ready_callback_ = callback; }

core/video_options.hpp
@@ -161,9 +161,11 @@ struct VideoOptions : public Options
 			("low-latency", value<bool>(&low_latency)->default_value(false)->implicit_value(true),
 			 "Enables the libav/libx264 low latency presets for video encoding.")
 #endif
+#ifndef DISABLE_RPI_FEATURES
 			 ("sync", value<std::string>(&sync_)->default_value("off"),
 			  "Whether to synchronise with another camera. Use \"off\", \"server\" or \"client\".")
-			;
+#endif
+		;
 		// clang-format on
 	}
 

meson.build
@@ -36,6 +36,10 @@ endif
 dl_dep = dependency('dl', required : true)
 libcamera_dep = dependency('libcamera', required : true)
 
+if get_option('disable_rpi_features') == true
+    cpp_arguments += '-DDISABLE_RPI_FEATURES'
+endif
+
 summary({
             'location' : libcamera_dep.get_variable('libdir'),
             'version' : libcamera_dep.version()

commit 5e8b59ae566a5bf0039dd59b6698357193fb5e1c

core: Update checks for disable_rpi_features option
Based on the original commit that added the sync feature there
were a few more options that needed the disable_rpi_features
code conditionals to ensure compilation against upstream
libcamera so add them in to ensure this works.

Fixes: c529096 ("core: Add "disable_rpi_features" meson option")
Signed-off-by: Peter Robinson <pbrobinson@gmail.com>

core/rpicam_encoder.hpp
@@ -51,10 +51,12 @@ public:
 	{
 		assert(encoder_);
 
+#ifndef DISABLE_RPI_FEATURES
 		// If sync was enabled, and SyncReady is still "false" then we must skip this frame. Tell our
 		// caller through the return value that we're not yet encoding anything.
 		if (GetOptions()->sync && !completed_request->metadata.get(controls::rpi::SyncReady).value_or(false))
 			return false;
+#endif
 
 		StreamInfo info = GetStreamInfo(stream);
 		FrameBuffer *buffer = completed_request->buffers[stream];

core/video_options.hpp
@@ -198,7 +198,9 @@ struct VideoOptions : public Options
 	size_t circular;
 	uint32_t frames;
 	bool low_latency;
+#ifndef DISABLE_RPI_FEATURES
 	uint32_t sync;
+#endif
 
 	virtual bool Parse(int argc, char *argv[]) override
 	{
@@ -243,6 +245,7 @@ struct VideoOptions : public Options
 			level = "4.2";
 		}
 
+#ifndef DISABLE_RPI_FEATURES
 		if (strcasecmp(sync_.c_str(), "off") == 0)
 			sync = 0;
 		else if (strcasecmp(sync_.c_str(), "server") == 0)
@@ -251,6 +254,7 @@ struct VideoOptions : public Options
 			sync = 2;
 		else
 			throw std::runtime_error("incorrect sync value " + sync_);
+#endif
 
 		return true;
 	}
@@ -271,7 +275,9 @@ struct VideoOptions : public Options
 		std::cerr << "    split: " << split << std::endl;
 		std::cerr << "    segment: " << segment << std::endl;
 		std::cerr << "    circular: " << circular << std::endl;
+#ifndef DISABLE_RPI_FEATURES
 		std::cerr << "    sync: " << sync << std::endl;
+#endif
 	}
 
 private:
@@ -280,5 +286,7 @@ private:
 	std::string av_sync_;
 	std::string audio_bitrate_;
 #endif /* LIBAV_PRESENT */
+#ifndef DISABLE_RPI_FEATURES
 	std::string sync_;
+#endif
 };